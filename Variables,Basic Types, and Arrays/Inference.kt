6b455baccf87be937bd0b01f1ddc2ecd56075bff
abhijitramesh <abhijitramesh2k@gmail.com>
2019-02-25 21:45:08 +0530

    Inference of datatypes

---
+++ Variables,Basic Types, and Arrays/Inference.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>){
+/*
+  *Kotlin has inbuild Inference or deduction of datatypes
+  *the compiler automatically predicts the datatypes entred by the programmer
+*/
+
+val country = "INDIA"
+val code = 192
+
+println("I am from $country \nmy postal code is $code")
+}
