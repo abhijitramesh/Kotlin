e5ef952307e2ad13a5d3c459b4d87d9d5f6abf16
abhijitramesh <abhijitramesh2k@gmail.com>
2019-02-26 01:29:40 +0530

    Kotlin basics

---
+++ Variables,Basic Types, and Arrays/Numbers.kt
@@ -0,0 +1,30 @@
+fun main(args: Array<String>){
+/*
+ * Kotlin consisers all data types as objects
+ * The Integer type availabe are
+                                  * Long 64 bit
+                                  * Int 32 bit
+                                  * Short 16 bit
+                                  * Byte 8 bit
+ * The Floating point type availabe are
+                                  * Double 64 bit
+                                  * Float 32 bit
+*/
+
+val myInt = 55
+val myLong = 44L
+val myFloat = 34.43F
+val myDouble = 45.78
+val myHex = 0x0F
+val myBinary = 0b10101
+
+println("The number $myInt is Int")
+println("The number $myLong is Long")
+println("The number $myFloat is Float")
+println("The number $myDouble is Double")
+println("The Hexadecimal $myHex is Hex")
+println("The Binary $myBinary is Binary")
+
+
+
+}
