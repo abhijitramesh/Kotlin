766ab7e7651d817dcd2baf02e3697f4158a2fedb
abhijitramesh <abhijitramesh2k@gmail.com>
2019-02-25 21:18:15 +0530

    Variables,Basic Types, and Arrays

---
+++ Variables,Basic Types, and Arrays/ValvsVar.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>){
+//val is is constant it is not mutable
+//var is variable it can be changed
+val DateOfBirth: String = "25th March 2000"//constant
+var dateofbirth: String = "27th May 1999"//variable
+
+println(DateOfBirth)
+println(dateofbirth)
+
+
+}
