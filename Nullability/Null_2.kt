47aa6534f086e90433879d773f522c2c9c01c2b3
abhijitramesh <abhijitramesh2k@gmail.com>
2019-02-26 20:24:34 +0530

    Null2

---
+++ Nullability/Null_2.kt
@@ -0,0 +1,12 @@
+fun main() {
+
+/*
+* Using the null safe Operator to envoke actions on variables
+* Declared as null
+*/
+
+
+val v: String? = null
+val length: Int ? = v?.length
+print(length)
+}
