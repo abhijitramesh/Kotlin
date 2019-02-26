6f00cdc25519468e08764ac2406249330d30841e
abhijitramesh <abhijitramesh2k@gmail.com>
2019-02-26 20:19:41 +0530

    Nullablity

---
+++ Nullability/Null_1.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>){
+
+/* ? is called the Safe Call Operator
+      it is used to invoke Safe Operations where a null
+      is invoked
+*/
+var name: String? = null
+println(name?.length)
+}
