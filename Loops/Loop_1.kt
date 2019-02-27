27899eb62c77f4aac81a0e633262c5fb837b6e42
abhijitramesh <abhijitramesh2k@gmail.com>
2019-02-27 11:55:37 +0530

    loops

---
+++ Loops/Loop_1.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>){
+
+var number = 20
+
+while(number > 0){
+println(" While loop Itration with number $number")
+number --
+
+}
+
+number = 20
+do{
+println("Do while loop Itration with number $number")
+number --
+}
+while(number > 0)
+for(Number in 5..15){
+
+println("This is Number $Number")
+}
+}
