22a4a1f3e4f1ab4db52cdeaeacbcf6c6d436ca25
abhijitramesh <abhijitramesh2k@gmail.com>
2019-02-27 17:35:08 +0530

    function of range example

---
+++ Ranges and Collections/Range_2.kt
@@ -0,0 +1,30 @@
+fun main(args: Array<String>){
+
+
+/*
+This is using rangeTo() function
+*/
+
+
+val oneToFive: IntRange = 1.rangeTo(5)
+
+
+for (n in oneToFive){
+
+println(n)
+
+
+/*
+This is using downTo() function
+*/
+
+
+
+val FiveToOne  = 5.downTo(1)
+
+for(e in FiveToOne)
+
+println(e)
+}
+
+}
