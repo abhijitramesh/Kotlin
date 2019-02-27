178f0757ea045187bed5687826f50e089452c53b
abhijitramesh <abhijitramesh2k@gmail.com>
2019-02-27 13:58:03 +0530

    traversing through array using indices

---
+++ Loops/Loop_5.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>){
+
+val numbers = intArrayOf(1,2,3,4,5,6,7,8,9)
+
+//index stores the index
+//numbers.indices is the indices limit
+
+for(index in numbers.indices){
+
+print("$index index value is ${numbers[index]}\n")
+
+
+}
+
+
+
+
+}
