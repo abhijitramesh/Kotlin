ad2bb6525f62d3b494be5dee924b865553b7b135
abhijitramesh <abhijitramesh2k@gmail.com>
2019-02-27 21:49:48 +0530

    List functions

---
+++ Ranges and Collections/List_2.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>){
+
+val mixed_list = listOf("TOM","Jerry",1,2.33,'s')
+
+println(mixed_list.size)
+println(mixed_list.get(0))
+println(mixed_list.indexOf("Jerry"))
+println(mixed_list.contains(1))
+
+
+}
