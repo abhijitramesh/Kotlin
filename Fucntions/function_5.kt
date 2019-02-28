78a112e679fa5245548b6fcedf2dbba613cee59c
abhijitramesh <abhijitramesh2k@gmail.com>
2019-02-28 17:27:00 +0530

    Default parameter

---
+++ Fucntions/function_5.kt
@@ -0,0 +1,9 @@
+fun name(First_name:String ,Middle_name:String = "", Last_Name:String){
+println("My Name is $First_name $Middle_name $Last_Name")
+}
+
+fun main(args: Array<String>) {
+
+name("Abhijit",Last_Name="Ramesh")
+
+}
