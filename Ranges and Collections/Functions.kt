75d2e9819cd87f9724516f8ea88d91cb48778a04
abhijitramesh <abhijitramesh2k@gmail.com>
2019-02-28 13:18:23 +0530

    functions on collections

---
+++ Ranges and Collections/Functions.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>){
+val numberSet: Set<Int> = setOf(1,2,3,4,5)
+
+println(numberSet.last())
+println(numberSet.first())
+println(numberSet.max())
+println(numberSet.drop(2))
+println(numberSet.plus(6))
+println(numberSet.minus(3))
+println(numberSet.average())
+
+
+}
