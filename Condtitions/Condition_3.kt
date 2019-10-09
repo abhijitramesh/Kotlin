61607b1f4e4f74383b1c6e3d94c9d387f7d3e55b
ipoopsometimes1 <56363289+ipoopsometimes1@users.noreply.github.com>
2019-10-09 23:30:08 +0530

    Update Condition_3.kt
    Dont merger but dont delete for 7 day atleast

--- Condtitions/Condition_3.kt
+++ Condtitions/Condition_3.kt
@@ -15,5 +15,64 @@ when (number.toInt()){
     //the buttmuncher was here
 }
 
+    val number = readLine()!!
+when (number.toInt()){
+    2 -> println("The number is 2")
+    1 -> println("The number is 1")
+    3 -> println("The number is 3")
+    4->printlmao("THIS IS TOTALLTY WRONG")
+    else -> println("The number is not 1 2 or 3")
+    //the buttmuncher was here
+}
+
+    val number = readLine()!!
+when (number.toInt()){
+    2 -> println("The number is 2")
+    1 -> println("The number is 1")
+    3 -> println("The number is 3")
+    4->printlmao("THIS IS TOTALLTY WRONG")
+    else -> println("The number is not 1 2 or 3")
+    //the buttmuncher was here
+}
+'val number = readLine()!!
+when (number.toInt()){
+    2 -> println("The number is 2")
+    1 -> println("The number is 1")
+    3 -> println("The number is 3")
+    4->printlmao("THIS IS TOTALLTY WRONG")
+    else -> println("The number is not 1 2 or 3")
+    //the buttmuncher was here
+}
+
+val number = readLine()!!
+when (number.toInt()){
+    2 -> println("The number is 2")
+    1 -> println("The number is 1")
+    3 -> println("The number is 3")
+    4->printlmao("THIS IS TOTALLTY WRONG")
+    else -> println("The number is not 1 2 or 3")
+    //the buttmuncher was here
+}
+
+val number = readLine()!!
+when (number.toInt()){
+    2 -> println("The number is 2")
+    1 -> println("The number is 1")
+    3 -> println("The number is 3")
+    4->printlmao("THIS IS TOTALLTY WRONG")
+    else -> println("The number is not 1 2 or 3")
+    //the buttmuncher was here
+}
+
+val number = readLine()!!
+when (number.toInt()){
+    2 -> println("The number is 2")
+    1 -> println("The number is 1")
+    3 -> println("The number is 3")
+    4->printlmao("THIS IS TOTALLTY WRONG")
+    else -> println("The number is not 1 2 or 3")
+    //the buttmuncher was here
+}
+
 
 }
