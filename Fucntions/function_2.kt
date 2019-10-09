911d8b238ea636db343c031bab09807bbad5d6f3
ipoopsometimes1 <56363289+ipoopsometimes1@users.noreply.github.com>
2019-10-09 23:26:24 +0530

    Update function_2.kt
    added stuff

--- Fucntions/function_2.kt
+++ Fucntions/function_2.kt
@@ -5,3 +5,9 @@ println("Hello $name")
 fun main(args: Array<String>){
 hello("John")
 }
+fun notmain(arg:Array<String>){
+  notmain("John is not the buttmuncher beware")
+}
+fun notmain2(arg:Array<String>){
+  notmain2("John is not the buttmuncher beware again")
+}
