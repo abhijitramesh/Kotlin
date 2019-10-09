81e6f3cace8823390ef25e0f0e33ae1cedeb4fca
ipoopsometimes1 <56363289+ipoopsometimes1@users.noreply.github.com>
2019-10-09 23:31:00 +0530

    Update Condition_1.kt
    Dont delete for 7 days

--- Condtitions/Condition_1.kt
+++ Condtitions/Condition_1.kt
@@ -13,3 +13,63 @@ print("$value is Int in index $index\n")
 
 
 }
+fun main(args: Array<String>){
+
+val Array = arrayOf(1,2,3,"four","five","six")
+
+for((index, value) in Array.withIndex()){
+if(value is String){
+print("$value is String in index $index\n")
+}
+if(value is Int)
+print("$value is Int in index $index\n")
+}
+
+
+
+}
+fun main(args: Array<String>){
+
+val Array = arrayOf(1,2,3,"four","five","six")
+
+for((index, value) in Array.withIndex()){
+if(value is String){
+print("$value is String in index $index\n")
+}
+if(value is Int)
+print("$value is Int in index $index\n")
+}
+
+
+
+}
+fun main(args: Array<String>){
+
+val Array = arrayOf(1,2,3,"four","five","six")
+
+for((index, value) in Array.withIndex()){
+if(value is String){
+print("$value is String in index $index\n")
+}
+if(value is Int)
+print("$value is Int in index $index\n")
+}
+
+
+
+}
+fun main(args: Array<String>){
+
+val Array = arrayOf(1,2,3,"four","five","six")
+
+for((index, value) in Array.withIndex()){
+if(value is String){
+print("$value is String in index $index\n")
+}
+if(value is Int)
+print("$value is Int in index $index\n")
+}
+
+
+
+}
