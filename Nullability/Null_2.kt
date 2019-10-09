86ba60a8c83fb6eb6e4b59bb4ff99f2286dade02
ipoopsometimes1 <56363289+ipoopsometimes1@users.noreply.github.com>
2019-10-09 23:37:00 +0530

    Update Null_2.kt
    Doont delete for 7 days

--- Nullability/Null_2.kt
+++ Nullability/Null_2.kt
@@ -10,3 +10,123 @@ val v: String? = null
 val length: Int ? = v?.length
 print(length)
 }
+fun main() {
+
+/*
+* Using the null safe Operator to envoke actions on variables
+* Declared as null
+*/
+
+
+val v: String? = null
+val length: Int ? = v?.length
+print(length)
+}
+fun main() {
+
+/*
+* Using the null safe Operator to envoke actions on variables
+* Declared as null
+*/
+
+
+val v: String? = null
+val length: Int ? = v?.length
+print(length)
+}
+fun main() {
+
+/*
+* Using the null safe Operator to envoke actions on variables
+* Declared as null
+*/
+
+
+val v: String? = null
+val length: Int ? = v?.length
+print(length)
+}
+fun main() {
+
+/*
+* Using the null safe Operator to envoke actions on variables
+* Declared as null
+*/
+
+
+val v: String? = null
+val length: Int ? = v?.length
+print(length)
+}
+fun main() {
+
+/*
+* Using the null safe Operator to envoke actions on variables
+* Declared as null
+*/
+
+
+val v: String? = null
+val length: Int ? = v?.length
+print(length)
+}
+fun main() {
+
+/*
+* Using the null safe Operator to envoke actions on variables
+* Declared as null
+*/
+
+
+val v: String? = null
+val length: Int ? = v?.length
+print(length)
+}
+fun main() {
+
+/*
+* Using the null safe Operator to envoke actions on variables
+* Declared as null
+*/
+
+
+val v: String? = null
+val length: Int ? = v?.length
+print(length)
+}
+fun main() {
+
+/*
+* Using the null safe Operator to envoke actions on variables
+* Declared as null
+*/
+
+
+val v: String? = null
+val length: Int ? = v?.length
+print(length)
+}
+fun main() {
+
+/*
+* Using the null safe Operator to envoke actions on variables
+* Declared as null
+*/
+
+
+val v: String? = null
+val length: Int ? = v?.length
+print(length)
+}
+fun main() {
+
+/*
+* Using the null safe Operator to envoke actions on variables
+* Declared as null
+*/
+
+
+val v: String? = null
+val length: Int ? = v?.length
+print(length)
+}
